6da769536802fd685f9dae88df369abc67b945b4
JustinValet <jv.datamail@gmail.com>
2019-05-16 20:23:10 +0200

    test

---
+++ ingestion_database/ingest_transactions/CleaningBeforeInsert.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import datetime
+
+# The aim of this function is to preprocess all of .csv files before
+# ingesting it into the associated table into the data base
+# The file is uploaded from .csv into a pandas DataFrame
+# This function return a pandas modified Dataframe
+# This function is used into the ingestion function of .csv file as
+# an optional argument
+
+
+def cleaning_before(Data,FilePath):
+
+    # Rename & Upper Case variables
+    Data.columns = Data.columns.str.replace(" ", "")
+
+    # Select variables to import
+
+    FileName = FilePath.split('/')[-1]
+
+    Continue = True
+
+    if len(FileName.split("_")) == 3:
+        DateFile = FileName.split("_")[1]
+        if len(DateFile) == 6:
+            DateFile = pd.to_datetime("20" + DateFile[4:6] + "-" + DateFile[2:4] + "-" + DateFile[0:2])
+        else:
+            Continue = False
+    else:
+        Continue = False
+
+    if Continue:
+
+        ### Filtering and cleaning ###
+        # Remove characters from string into the dataset
+        for var in Data.columns[Data.dtypes == 'object']:
+            Data[var] = Data[var].str.replace("'", "")
+
+        # Filtering by Transaction TP
+        Data = Data[~Data["TransactionTP"].isin(['ATM Domestic Auth', 'ATM International Auth',
+                                                 'POS Domestic Auth', 'POS International Auth', 'FX Fee'])]
+
+        # Reset the index
+        Data = Data.reset_index(drop=True)
+
+        # Combine Date & Time
+        Data = pd.concat(
+            [Data, pd.DataFrame(Data.Date.astype(str) + " " + Data.Time.astype(str), columns=['TransactionTime'])],
+            axis=1)
+        Data["TransactionTime"] = pd.to_datetime(Data.TransactionTime, errors='coerce')
+
+        # Filtering by Date
+        # convert to Date to datetime
+        Data["Date"] = pd.to_datetime(Data["Date"], errors='coerce')
+        Data = Data[Data["Date"] == (DateFile - datetime.timedelta(days=1))]
+        # Reset the index
+        Data = Data.reset_index(drop=True)
+
+        # Change Amount sign after 2018-04-11
+        if DateFile >= pd.to_datetime('2018-04-11'):
+            Data.loc[Data["TransactionTP"].isin(
+                ['ATM Domestic', 'ATM International', 'Merchandise Refund Hold Reversals', 'Merchant refunds',
+                 'POS Domestic', 'POS International']), ["Amount"]] = - Data["Amount"]
+
+        # Drop Data, Time & First Name
+        Data = Data.drop(columns=["Date", "Time", "FirstName"])
+
+        CardType = FileName
+        if FileName.split("_")[0] == "PCSCHROMEEURRECONCILIATIONDETAILV2":
+            CardType = "CHROME"
+        elif FileName.split("_")[0] == "PCSFRANCEEURRECONCILIATIONDETAILV2":
+            CardType = "BLACK"
+        elif FileName.split("_")[0] == "PCSINFINITYEURRECONCILIATIONDETAILV2":
+            CardType = "INFINITY"
+        else:
+            CardType = FileName
+
+        Data["CardType"] = CardType
+
+        # Reorder variables in order to always insure the order of variables when ingesting
+        Data = Data[sorted(Data.columns)]
+
+    else:
+        Data = None
+
+    return Data
---
+++ ingestion_database/ingest_transactions/csv_transactions.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from BDDTools import Ingestion
+from ingestion_database.ingest_transactions import CleaningBeforeInsert
+import numpy as np
+import os
+import glob
+import datetime
+import time
+
+
+def ingest_csv_transactions(credentials, Schema, Folder, **kwargs):
+
+    logger = kwargs.get('logger')
+
+    # Create the dictionnary in order to insure you
+    # that at each ingestion of .csv file
+    # you keep the same data type into the
+    # database
+    TableParameter = {}
+    TableParameter["Amount"] = "double precision"
+    TableParameter["AuthNum"] = "INTEGER"
+    TableParameter["CardHolderID"] = "VARCHAR (50)"
+    TableParameter["CardNumber"] = "VARCHAR (50)"
+    TableParameter["CardProductName"] = "double precision"
+    TableParameter["CardType"] = "VARCHAR (20)"
+    TableParameter["CardVPUType"] = "VARCHAR (50)"
+    TableParameter["Currency"] = "VARCHAR (20)"
+    TableParameter["CurrencyCode"] = "INTEGER"
+    TableParameter["DebitCredit"] = "VARCHAR (20)"
+    TableParameter["Description"] = "TEXT"
+    TableParameter["DistributorCode"] = "INTEGER"
+    TableParameter["Fee"] = "double precision"
+    TableParameter["LastName"] = "VARCHAR (50)"
+    TableParameter["MCC"] = "TEXT"
+    TableParameter["MerchantAddress"] = "TEXT"
+    TableParameter["MerchantCity"] = "TEXT"
+    TableParameter["MerchantCountry"] = "TEXT"
+    TableParameter["MerchantID"] = "VARCHAR(50)"
+    TableParameter["MerchantName"] = "TEXT"
+    TableParameter["ProgramName"] = "VARCHAR(50)"
+    TableParameter["PurseCurrency"] = "double precision"
+    TableParameter["Rate"] = "double precision"
+    TableParameter["RemainingBalance"] = "double precision"
+    TableParameter["Surcharge"] = "double precision"
+    TableParameter["SweepCurrency"] = "VARCHAR(20)"
+    TableParameter["TermState"] = "TEXT"
+    TableParameter["TransactionDB/CR"] = "VARCHAR (40)"
+    TableParameter["TransactionID"] = "VARCHAR (40)"
+    TableParameter["TransactionResult"] = "VARCHAR (40)"
+    TableParameter["TransactionTP"] = "VARCHAR (60)"
+    TableParameter["TransactionTime"] = "timestamp without time zone"
+
+    # Create the list of files that must be
+    # ingested into the data base
+    # I suggest created different folder for the different month &
+    # dayli ingestion
+    os.chdir(Folder)
+    ListOfFile = glob.glob('**.csv')
+    ListOfFile = pd.DataFrame(ListOfFile, columns=["FileName"])
+    Month = []
+    Year = []
+    Day = []
+    # create a pandas DataFrame that contain
+    # file and associated year and month in order
+    # to ingest .csv file for each month
+    ii = ListOfFile.FileName.str.split('_')
+    for i in range(0, len(ii)):
+        Day.append(ii[i][1][0:2])
+        Month.append(ii[i][1][2:4])
+        Year.append("20" + ii[i][1][4:6])
+
+    ListOfFile = pd.concat([ListOfFile, pd.DataFrame(Year, columns=['Year']), pd.DataFrame(Month, columns=['Month']),
+                            pd.DataFrame(Day, columns=['Day'])],
+                           axis=1)
+
+    ListOfFile["FileTime"] = ListOfFile["Year"] + "-" + ListOfFile["Month"] + "-" + ListOfFile["Day"]
+    ListOfFile["FileTime"] = pd.to_datetime(ListOfFile.FileTime, errors='coerce')
+    ListOfFile["FileTimeDelta"] = ListOfFile["FileTime"] - datetime.timedelta(days=1)
+
+    ListOfFile["Year"] = ListOfFile.FileTimeDelta.dt.year
+    ListOfFile["Month"] = ListOfFile.FileTimeDelta.dt.month
+
+    tic = time.time()
+    ListToIterate = list(np.unique(ListOfFile.Year.astype(str) + "_" + ListOfFile.Month.astype(str)))
+
+    # Function for preprocessing the data before the ingestion
+    # This is a dictionnary that takes in arguments
+    # - function to preprocessing the data (output dataframe)
+    # - KeysWords of optinal argument for the function
+    FinalKeywords = dict(function=CleaningBeforeInsert.cleaning_before, KeyWords=None)
+
+    # iterate on each month in order to create / ingest
+    # .csv dayli files into the associated monthly table
+    for k in range(0, len(ListToIterate)):
+        ListFinal = list(ListOfFile["FileName"][(ListOfFile.Month == int(ListToIterate[k].split('_')[1])) & (
+                    ListOfFile.Year == int(ListToIterate[k].split('_')[0]))])
+        # Name of the table
+        TlbNameMonth = "MONTHLY_TRANSACTIONS_" + ListToIterate[k].split('_')[0] + ListToIterate[k].split('_')[1]
+        # Ingestion part
+        Ingestion.FromCsvToDataBase(ListFinal, credentials, Schema, TlbName=TlbNameMonth, logger=logger,
+                                    TableDict=TableParameter, InsertInTheSameTable=True,
+                                    InsertInParrell=True, PreprocessingCsv=FinalKeywords)
+
+    toc = time.time() - tic
+
+    print("The ingestion was done in {} seconds".format(toc))
