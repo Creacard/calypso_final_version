fb1a8c3f2557f2ed9451c4441eedb6d5d9bd4f58
JustinValet <jv.datamail@gmail.com>
2019-05-19 10:39:10 +0200

    test

--- ingestion_database/ingestion_database/DayliCsvLauncher.py
+++ ingestion_database/ingestion_database/DayliCsvLauncher.py
@@ -1,5 +1,5 @@
 from Utils import LogErrorFromInsert
-from ingest_transactions import csv_transactions
+from ingestion_database.ingest_transactions import csv_transactions
 
 credentials = dict()
 credentials["user"] = "justin"
@@ -9,7 +9,7 @@ credentials["pwd"] = "Thekiller81"
 # Create the logger in order to log
 # possible errors that could be generated
 # during the ingestion
-logger = LogErrorFromInsert.CreateLogger("DayliTransaction", "Ingestion_",'/Users/justinvalet/tmp/Log/')
+logger = LogErrorFromInsert.CreateLogger("DayliTransaction", "Ingestion_", '/Users/justinvalet/tmp/Log/')
 
 
 # Indicate the folder on where .csv file are located
