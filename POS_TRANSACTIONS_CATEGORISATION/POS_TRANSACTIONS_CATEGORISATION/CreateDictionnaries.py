2c6278c66853e0a4534bda9ee45c35931f186f07
JustinValet <jv.datamail@gmail.com>
2019-06-01 13:45:41 +0200

    test

--- POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/CreateDictionnaries.py
+++ POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/CreateDictionnaries.py
@@ -1,117 +1,116 @@
 import pandas as pd
-from BDDTools.DbTools import InsterToPostgre
-import numpy as np
-
-# Database connection
+from BDDTools.DbTools import InsterToPostgre,CreateSchema
 from BDDTools.DataBaseEngine import ConnectToPostgres
+import os
+import sys
+
+def create_update_dictionnaries_categorisation(credentials):
 
 
-Schema = 'MONTHLY_DATAMDL'
-user = 'justin'
-pwd = 'Thekiller81'
-hostname = '127.0.0.1'
-port = '26026'
-database = 'Creacard'
+    PostgresConnect = ConnectToPostgres(credentials)
+    engine = PostgresConnect.CreateEngine()
 
-PostgresConnect = ConnectToPostgres(user=user, pwd=pwd, hostname=hostname, port=port, database=database)
-PostgresConnect = PostgresConnect.CreateEngine()
-engine = PostgresConnect._openConnection
+    CreateSchema(engine, "REFERENTIEL")
 
+    # Conf files location
+    if sys.platform == "win32":
+        Folder = os.path.expanduser['~'] + "\\conf_python\\categorisation_univers\\"
+    else:
+        Folder = os.environ['HOME'] + "/conf_python/categorisation_univers/"
 
-Folder = "/Users/justinvalet/CreacardProject/Dictionnaires/"
+    # Referentials table
+    FileDescription = "description_univers.xlsx"
 
-# Referentials table
-FileDescription = "description_univers.xlsx"
+    DataDescritpion = pd.read_excel(Folder+FileDescription)
 
-DataDescritpion = pd.read_excel(Folder+FileDescription)
+    TableParameter = {}
+    TableParameter["UNIVERS_DATABASE"]      = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS_DATABASE"] = "VARCHAR (100)"
+    TableParameter["UNIVERS"]               = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
+    TableParameter["DESCRIPTION"]           = "TEXT"
 
-TableParameter = {}
-TableParameter["UNIVERS_DATABASE"]      = "VARCHAR (50)"
-TableParameter["SOUS_UNIVERS_DATABASE"] = "VARCHAR (100)"
-TableParameter["UNIVERS"]               = "VARCHAR (50)"
-TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
-TableParameter["DESCRIPTION"]           = "TEXT"
+    InsterToPostgre(DataDescritpion, engine, "UNIVERS_DESCRIPTION", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
 
-InsterToPostgre(DataDescritpion, engine, "UNIVERS_DESCRIPTION", "REFERENTIEL", DropTable=True,TableDict=TableParameter)
+    del DataDescritpion
+    del FileDescription
 
-del DataDescritpion
-del FileDescription
+    # link between MCC and MCC_CODE
+    FileMCC = "mcc_code_link.xlsx"
 
-# link between MCC and MCC_CODE
-FileMCC = "mcc_code_link.xlsx"
+    DataFileMCC = pd.read_excel(Folder+FileMCC, dtype={'MCC_CODE': str, 'MCC': object})
 
-DataFileMCC = pd.read_excel(Folder+FileMCC, dtype={'MCC_CODE': str, 'MCC': object})
+    TableParameter = {}
+    TableParameter["MCC_CODE"] = "VARCHAR (20)"
+    TableParameter["MCC"]      = "TEXT"
 
-TableParameter = {}
-TableParameter["MCC_CODE"] = "VARCHAR (20)"
-TableParameter["MCC"]      = "TEXT"
-
-InsterToPostgre(DataFileMCC, engine, "MCC_CODE_LINK", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
-del FileMCC
-del DataFileMCC
+    InsterToPostgre(DataFileMCC, engine, "MCC_CODE_LINK", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    del FileMCC
+    del DataFileMCC
 
 
-# Ingest MCC_CATEGORIES
-FileMCCCat = "mcc_categories.xlsx"
+    # Ingest MCC_CATEGORIES
+    FileMCCCat = "mcc_categories.xlsx"
 
-DataMCCCat = pd.read_excel(Folder+FileMCCCat)
+    DataMCCCat = pd.read_excel(Folder+FileMCCCat)
 
-TableParameter = {}
-TableParameter["MCC_NAME"]              = "TEXT"
-TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
-TableParameter["UNIVERS"]               = "VARCHAR (50)"
-TableParameter["MCC_CODE"]              = "VARCHAR (20)"
-TableParameter["NOTE"]                  = "INTEGER"
+    TableParameter = {}
+    TableParameter["MCC_NAME"]              = "TEXT"
+    TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
+    TableParameter["UNIVERS"]               = "VARCHAR (50)"
+    TableParameter["MCC_CODE"]              = "VARCHAR (20)"
+    TableParameter["NOTE"]                  = "INTEGER"
 
-InsterToPostgre(DataMCCCat,engine, "MCC_CATEGORIES", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
-del FileMCCCat
-del DataMCCCat
-
-
-# Regex exclusion
-FileRegexExclu = "regex_merchant.xlsx"
-
-Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex exclu')
-Data1 = Data1[~Data1.UNIVERS.isna()]
-TableParameter = {}
-TableParameter["MCC"]              = "TEXT"
-TableParameter["Regex"]            = "TEXT"
-TableParameter["UNIVERS"]          = "VARCHAR (50)"
-TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
-TableParameter["NEW_REGEX"]        = "TEXT"
-TableParameter["MCC_CODE"]         = "VARCHAR (20)"
-
-InsterToPostgre(Data1,engine, "REGEX_EXCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
-del FileRegexExclu
-del Data1
+    InsterToPostgre(DataMCCCat,engine, "MCC_CATEGORIES", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    del FileMCCCat
+    del DataMCCCat
 
 
-# Update MCC code for Regex excluded
+    # Regex exclusion
+    FileRegexExclu = "regex_merchant.xlsx"
 
-query = """
-UPDATE "REFERENTIEL"."REGEX_EXCLUDED"
-set "MCC_CODE" = T2."MCC_CODE"
-FROM "REFERENTIEL"."MCC_CATEGORIES" AS T2
-where "MCC" = T2."MCC_NAME"
-"""
+    Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex exclu')
+    Data1 = Data1[~Data1.UNIVERS.isna()]
+    TableParameter = {}
+    TableParameter["MCC"]              = "TEXT"
+    TableParameter["Regex"]            = "TEXT"
+    TableParameter["UNIVERS"]          = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
+    TableParameter["NEW_REGEX"]        = "TEXT"
+    TableParameter["MCC_CODE"]         = "VARCHAR (20)"
 
-engine.execute(query)
+    InsterToPostgre(Data1,engine, "REGEX_EXCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    del FileRegexExclu
+    del Data1
 
-# Regex inclusion
-FileRegexExclu = "regex_ajout.xlsx"
 
-Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex ajout')
-Data1 = Data1[~Data1.UNIVERS.isna()]
-TableParameter = {}
-TableParameter["Regex"]            = "TEXT"
-TableParameter["UNIVERS"]          = "VARCHAR (50)"
-TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
-TableParameter["NEW_REGEX"]        = "TEXT"
+    # Update MCC code for Regex excluded
 
-InsterToPostgre(Data1, engine, "REGEX_INCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
-del FileRegexExclu
-del Data1
+    query = """
+    UPDATE "REFERENTIEL"."REGEX_EXCLUDED"
+    set "MCC_CODE" = T2."MCC_CODE"
+    FROM "REFERENTIEL"."MCC_CATEGORIES" AS T2
+    where "MCC" = T2."MCC_NAME"
+    """
 
+    engine.execute(query)
+
+    # Regex inclusion
+    FileRegexExclu = "regex_ajout.xlsx"
+
+    Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex ajout')
+    Data1 = Data1[~Data1.UNIVERS.isna()]
+    TableParameter = {}
+    TableParameter["Regex"]            = "TEXT"
+    TableParameter["UNIVERS"]          = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
+    TableParameter["NEW_REGEX"]        = "TEXT"
+
+    InsterToPostgre(Data1, engine, "REGEX_INCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    del FileRegexExclu
+    del Data1
+
+    engine.close()
 
 
 
--- POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/LaunchRegexExcluded.py
+++ POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/LaunchRegexExcluded.py
@@ -3,143 +3,170 @@ import pandas as pd
 # Database connection
 from BDDTools.DataBaseEngine import ConnectToPostgres
 import time
-from multiprocessing.dummy import Pool as ThreadPool
-from functools import partial
-from POS_TRANSACTIONS_CATEGORISATION import RegexExcluded
 
+def ExcludedRegex(NumRow, DataRegex, engine, TlbName, schema):
+    # Step 2.2 looping update
+        query = """
+        UPDATE "{}"."{}"
+        SET "UNIVERS" = '{}', 
+        "SOUS_UNIVERS" = '{}'
+        WHERE "MCC" IN ('{}') AND 
+        "MerchantName" ~* '{}' 
+        """.format(TlbName,schema,str(DataRegex.loc[NumRow, "UNIVERS_DATABASE"].encode('ascii', 'ignore')),
+                   str(DataRegex.loc[NumRow, "SOUS_UNIVERS_DATABASE"].encode('ascii', 'ignore')),
+                   DataRegex.loc[NumRow, "MCC_DATABASE"].encode('ascii', 'ignore').replace("'", "''"),
+                   DataRegex.loc[NumRow, "REGEX"].encode('ascii', 'ignore').replace("'", "''"))
 
-user = 'justin'
-pwd = 'Thekiller81'
-hostname = '127.0.0.1'
-port = '26026'
-database = 'Creacard'
+        engine.execute(query)
+        print(NumRow)
 
-PostgresConnect = ConnectToPostgres(user=user, pwd=pwd, hostname=hostname, port=port, database=database)
-PostgresConnect = PostgresConnect.CreateEngine()
-engine = PostgresConnect._openConnection
+def IncludedRegex(NumRow, DataRegex, engine, TlbName, schema):
+    # Step 2.2 looping update
+        query = """
+        UPDATE "{}"."{}"
+        SET "UNIVERS" = '{}', 
+        "SOUS_UNIVERS" = '{}'
+        WHERE "UNIVERS" = '' and "SOUS_UNIVERS" = '' and "MerchantName" ~* '{}' 
+        """.format(TlbName, schema, str(DataRegex.loc[NumRow, "UNIVERS_DATABASE"].encode('ascii', 'ignore')),
+                   str(DataRegex.loc[NumRow, "SOUS_UNIVERS_DATABASE"].encode('ascii', 'ignore')),
+                   DataRegex.loc[NumRow, "REGEX"].encode('ascii', 'ignore').replace("'", "''"))
 
-
-
-# Step 1.1 - MCC code note 1 -- update categories and under categories
-query = """
-UPDATE "TRANSACTIONS"."POS_TRANSACTIONS"
-SET "UNIVERS" = T4."UNIVERS_DATABASE",
-	"SOUS_UNIVERS" = T4."SOUS_UNIVERS_DATABASE"
-from(
-	-- Join MCC categories, univers database & MCC database 
-	select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE" 
-	from "REFERENTIEL"."MCC_CATEGORIES" as T1 
-	INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
-	ON T1."MCC_CODE" = T2."MCC_CODE"
-	INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
-	ON T1."UNIVERS" = T3."UNIVERS" and 
-	T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
-	WHERE T1."NOTE" in ('1')
-	) as T4
-WHERE T4."MCC_DATABASE" = "MCC"
-"""
-tic = time.time()
-engine.execute(query)
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
-
-# Step 1.2 - MCC code note 1 -- Regex excluded
-query = """
-    select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE",T1."NEW_REGEX" as "REGEX"
-    from "REFERENTIEL"."REGEX_EXCLUDED" as T1 
-    INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
-    ON T1."MCC_CODE" = T2."MCC_CODE"
-    INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
-    ON T1."UNIVERS" = T3."UNIVERS" and 
-    T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
-	INNER JOIN "REFERENTIEL"."MCC_CATEGORIES" as T4
-	ON T1."MCC_CODE" = T4."MCC_CODE"
-	WHERE T4."NOTE" = 1  
-"""
-
-DataRegex = pd.read_sql(query, con=engine)
-
-# Step 2.2 looping update
-#tic = time.time()
-#p = ThreadPool(3)
-#p.map(
-#    partial(RegexExcluded.ExcludedRegex, DataRegex=DataRegex, engine=engine), range(0, len(DataRegex)))
-#p.close()
-#p.join()
-
-
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
+        engine.execute(query)
+        print(NumRow)
 
 
 
-tic = time.time()
-for i in range(0,len(DataRegex)):
-    print(i)
-    RegexExcluded.ExcludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine)
+def fill_univers_sous_univers(credentials, schema, TlbName):
 
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
+    PostgresConnect = ConnectToPostgres(credentials)
+    engine = PostgresConnect.CreateEngine()
 
-# Step 2.1 - MCC code note 0 and 2 -- update categories and under categories
-query = """
-UPDATE "TRANSACTIONS"."POS_TRANSACTIONS"
-SET "UNIVERS" = T4."UNIVERS_DATABASE",
-	"SOUS_UNIVERS" = T4."SOUS_UNIVERS_DATABASE"
-from(
-	-- Join MCC categories, univers database & MCC database 
-	select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE" 
-	from "REFERENTIEL"."MCC_CATEGORIES" as T1 
-	INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
-	ON T1."MCC_CODE" = T2."MCC_CODE"
-	INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
-	ON T1."UNIVERS" = T3."UNIVERS" and 
-	T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
-	WHERE T1."NOTE" in ('2','0')
-	) as T4
-WHERE T4."MCC_DATABASE" = "MCC"
-"""
-tic = time.time()
-engine.execute(query)
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
 
-# Step 2.2 - MCC code note 1 -- Regex excluded
-query = """
-    select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE",T1."NEW_REGEX" as "REGEX"
-    from "REFERENTIEL"."REGEX_EXCLUDED" as T1 
-    INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
-    ON T1."MCC_CODE" = T2."MCC_CODE"
-    INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
-    ON T1."UNIVERS" = T3."UNIVERS" and 
-    T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
-	INNER JOIN "REFERENTIEL"."MCC_CATEGORIES" as T4
-	ON T1."MCC_CODE" = T4."MCC_CODE"
-	WHERE T4."NOTE" in ('0','2')
-"""
 
-DataRegex = pd.read_sql(query, con=engine)
+    # Step 1.1 - MCC code note 1 -- update categories and under categories
+    query = """
+    UPDATE "{}"."{}"
+    SET "UNIVERS" = T4."UNIVERS_DATABASE",
+        "SOUS_UNIVERS" = T4."SOUS_UNIVERS_DATABASE"
+    from(
+        -- Join MCC categories, univers database & MCC database 
+        select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE" 
+        from "REFERENTIEL"."MCC_CATEGORIES" as T1 
+        INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
+        ON T1."MCC_CODE" = T2."MCC_CODE"
+        INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
+        ON T1."UNIVERS" = T3."UNIVERS" and 
+        T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
+        WHERE T1."NOTE" in ('1')
+        ) as T4
+    WHERE T4."MCC_DATABASE" = "MCC"
+    """.format(schema,TlbName)
 
-tic = time.time()
-for i in range(0,len(DataRegex)):
-    print(i)
-    RegexExcluded.ExcludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine)
+    tic = time.time()
+    engine.execute(query)
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
 
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
+    # Step 1.2 - MCC code note 1 -- Regex excluded
+    query = """
+        select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE",T1."NEW_REGEX" as "REGEX"
+        from "REFERENTIEL"."REGEX_EXCLUDED" as T1 
+        INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
+        ON T1."MCC_CODE" = T2."MCC_CODE"
+        INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
+        ON T1."UNIVERS" = T3."UNIVERS" and 
+        T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
+        INNER JOIN "REFERENTIEL"."MCC_CATEGORIES" as T4
+        ON T1."MCC_CODE" = T4."MCC_CODE"
+        WHERE T4."NOTE" = 1  
+    """
 
-# step 3 - regex adding
+    DataRegex = pd.read_sql(query, con=engine)
 
-# Step 3.1 - MCC code note 1 -- Regex excluded
-query = """
-  select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",
-	T1."NEW_REGEX" as "REGEX"
-    from "REFERENTIEL"."REGEX_INCLUDED" as T1 
-    INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
-    ON T1."UNIVERS" = T3."UNIVERS" and 
-    T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS" 
-"""
+    # Step 2.2 looping update
+    #tic = time.time()
+    #p = ThreadPool(3)
+    #p.map(
+    #    partial(RegexExcluded.ExcludedRegex, DataRegex=DataRegex, engine=engine), range(0, len(DataRegex)))
+    #p.close()
+    #p.join()
 
-DataRegex = pd.read_sql(query, con=engine)
 
-tic = time.time()
-for i in range(0,len(DataRegex)):
-    print(i)
-    RegexExcluded.IncludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine)
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
 
-print("update of exclusion was done in {} seconds".format(time.time() - tic))
+
+
+    tic = time.time()
+    for i in range(0,len(DataRegex)):
+        print(i)
+        ExcludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine, TlbName=TlbName, schema=schema)
+
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
+
+    # Step 2.1 - MCC code note 0 and 2 -- update categories and under categories
+    query = """
+    UPDATE "{}"."{}"
+    SET "UNIVERS" = T4."UNIVERS_DATABASE",
+        "SOUS_UNIVERS" = T4."SOUS_UNIVERS_DATABASE"
+    from(
+        -- Join MCC categories, univers database & MCC database 
+        select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE" 
+        from "REFERENTIEL"."MCC_CATEGORIES" as T1 
+        INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
+        ON T1."MCC_CODE" = T2."MCC_CODE"
+        INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
+        ON T1."UNIVERS" = T3."UNIVERS" and 
+        T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
+        WHERE T1."NOTE" in ('2','0')
+        ) as T4
+    WHERE T4."MCC_DATABASE" = "MCC"
+    """.format(schema,TlbName)
+
+    tic = time.time()
+    engine.execute(query)
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
+
+    # Step 2.2 - MCC code note 1 -- Regex excluded
+    query = """
+        select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",T2."MCC" as "MCC_DATABASE",T1."NEW_REGEX" as "REGEX"
+        from "REFERENTIEL"."REGEX_EXCLUDED" as T1 
+        INNER JOIN "REFERENTIEL"."MCC_CODE_LINK" as T2 
+        ON T1."MCC_CODE" = T2."MCC_CODE"
+        INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
+        ON T1."UNIVERS" = T3."UNIVERS" and 
+        T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS"
+        INNER JOIN "REFERENTIEL"."MCC_CATEGORIES" as T4
+        ON T1."MCC_CODE" = T4."MCC_CODE"
+        WHERE T4."NOTE" in ('0','2')
+    """
+
+    DataRegex = pd.read_sql(query, con=engine)
+
+    tic = time.time()
+    for i in range(0,len(DataRegex)):
+        print(i)
+        ExcludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine, TlbName=TlbName, schema=schema)
+
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
+
+    # step 3 - regex adding
+
+    # Step 3.1 - MCC code note 1 -- Regex excluded
+    query = """
+      select T3."UNIVERS_DATABASE",T3."SOUS_UNIVERS_DATABASE",
+        T1."NEW_REGEX" as "REGEX"
+        from "REFERENTIEL"."REGEX_INCLUDED" as T1 
+        INNER JOIN "REFERENTIEL"."UNIVERS_DESCRIPTION" as T3
+        ON T1."UNIVERS" = T3."UNIVERS" and 
+        T1."SOUS_UNIVERS" = T3."SOUS_UNIVERS" 
+    """
+
+    DataRegex = pd.read_sql(query, con=engine)
+
+    tic = time.time()
+    for i in range(0, len(DataRegex)):
+        print(i)
+        IncludedRegex(NumRow=i, DataRegex=DataRegex, engine=engine, TlbName=TlbName, schema=schema)
+
+    print("update of exclusion was done in {} seconds".format(time.time() - tic))
+
+    engine.close()
