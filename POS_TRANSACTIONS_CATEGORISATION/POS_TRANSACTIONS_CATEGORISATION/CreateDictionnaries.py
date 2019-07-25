e9d541f3fc70b51d841f04d4c651ed98aa8973e1
JustinValet <jv.datamail@gmail.com>
2019-07-25 11:36:08 +0200

    test

--- POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/CreateDictionnaries.py
+++ POS_TRANSACTIONS_CATEGORISATION/POS_TRANSACTIONS_CATEGORISATION/CreateDictionnaries.py
@@ -3,8 +3,10 @@ from Postgres_Toolsbox.DbTools import InsertToPostgre, CreateSchema
 from creacard_connectors.database_connector import connect_to_database
 import os
 import sys
+from Postgres_Toolsbox.Ingestion import InsertTableIntoDatabase
 
 def create_update_dictionnaries_categorisation(database_type,database_name):
+
 
 
     engine = connect_to_database(database_type, database_name).CreateEngine()
@@ -20,16 +22,21 @@ def create_update_dictionnaries_categorisation(database_type,database_name):
     # Referentials table
     FileDescription = "description_univers.xlsx"
 
-    DataDescritpion = pd.read_excel(Folder+FileDescription)
+    DataDescritpion = pd.read_excel(Folder + FileDescription)
 
     TableParameter = {}
-    TableParameter["UNIVERS_DATABASE"]      = "VARCHAR (50)"
+    TableParameter["UNIVERS_DATABASE"] = "VARCHAR (50)"
     TableParameter["SOUS_UNIVERS_DATABASE"] = "VARCHAR (100)"
-    TableParameter["UNIVERS"]               = "VARCHAR (50)"
-    TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
-    TableParameter["DESCRIPTION"]           = "TEXT"
+    TableParameter["UNIVERS"] = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"] = "VARCHAR (100)"
+    TableParameter["DESCRIPTION"] = "TEXT"
 
-    InsertToPostgre(DataDescritpion, engine, "UNIVERS_DESCRIPTION", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    InsertTableIntoDatabase(DataDescritpion,
+                            "UNIVERS_DESCRIPTION",
+                            "REFERENTIEL",
+                            database_type, database_name,
+                            DropTable=True,
+                            TableDict=TableParameter)
 
     del DataDescritpion
     del FileDescription
@@ -37,51 +44,66 @@ def create_update_dictionnaries_categorisation(database_type,database_name):
     # link between MCC and MCC_CODE
     FileMCC = "mcc_code_link.xlsx"
 
-    DataFileMCC = pd.read_excel(Folder+FileMCC, dtype={'MCC_CODE': str, 'MCC': object})
+    DataFileMCC = pd.read_excel(Folder + FileMCC, dtype={'MCC_CODE': str, 'MCC': object})
 
     TableParameter = {}
     TableParameter["MCC_CODE"] = "VARCHAR (20)"
-    TableParameter["MCC"]      = "TEXT"
+    TableParameter["MCC"] = "TEXT"
 
-    InsertToPostgre(DataFileMCC, engine, "MCC_CODE_LINK", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    InsertTableIntoDatabase(DataFileMCC,
+                            "MCC_CODE_LINK",
+                            "REFERENTIEL",
+                            database_type, database_name,
+                            DropTable=True,
+                            TableDict=TableParameter)
+
     del FileMCC
     del DataFileMCC
-
 
     # Ingest MCC_CATEGORIES
     FileMCCCat = "mcc_categories.xlsx"
 
-    DataMCCCat = pd.read_excel(Folder+FileMCCCat)
+    DataMCCCat = pd.read_excel(Folder + FileMCCCat)
 
     TableParameter = {}
-    TableParameter["MCC_NAME"]              = "TEXT"
-    TableParameter["SOUS_UNIVERS"]          = "VARCHAR (100)"
-    TableParameter["UNIVERS"]               = "VARCHAR (50)"
-    TableParameter["MCC_CODE"]              = "VARCHAR (20)"
-    TableParameter["NOTE"]                  = "INTEGER"
+    TableParameter["MCC_NAME"] = "TEXT"
+    TableParameter["SOUS_UNIVERS"] = "VARCHAR (100)"
+    TableParameter["UNIVERS"] = "VARCHAR (50)"
+    TableParameter["MCC_CODE"] = "VARCHAR (20)"
+    TableParameter["NOTE"] = "INTEGER"
 
-    InsertToPostgre(DataMCCCat,engine, "MCC_CATEGORIES", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    InsertTableIntoDatabase(DataMCCCat,
+                            "MCC_CATEGORIES",
+                            "REFERENTIEL",
+                            database_type, database_name,
+                            DropTable=True,
+                            TableDict=TableParameter)
+
     del FileMCCCat
     del DataMCCCat
-
 
     # Regex exclusion
     FileRegexExclu = "regex_merchant.xlsx"
 
-    Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex exclu')
+    Data1 = pd.read_excel(Folder + FileRegexExclu, sheet_name='Regex exclu')
     Data1 = Data1[~Data1.UNIVERS.isna()]
     TableParameter = {}
-    TableParameter["MCC"]              = "TEXT"
-    TableParameter["Regex"]            = "TEXT"
-    TableParameter["UNIVERS"]          = "VARCHAR (50)"
-    TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
-    TableParameter["NEW_REGEX"]        = "TEXT"
-    TableParameter["MCC_CODE"]         = "VARCHAR (20)"
+    TableParameter["MCC"] = "TEXT"
+    TableParameter["Regex"] = "TEXT"
+    TableParameter["UNIVERS"] = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"] = "VARCHAR (100)"
+    TableParameter["NEW_REGEX"] = "TEXT"
+    TableParameter["MCC_CODE"] = "VARCHAR (20)"
 
-    InsertToPostgre(Data1,engine, "REGEX_EXCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    InsertTableIntoDatabase(Data1,
+                            "REGEX_EXCLUDED",
+                            "REFERENTIEL",
+                            database_type, database_name,
+                            DropTable=True,
+                            TableDict=TableParameter)
+
     del FileRegexExclu
     del Data1
-
 
     # Update MCC code for Regex excluded
 
@@ -97,22 +119,22 @@ def create_update_dictionnaries_categorisation(database_type,database_name):
     # Regex inclusion
     FileRegexExclu = "regex_ajout.xlsx"
 
-    Data1 = pd.read_excel(Folder+FileRegexExclu, sheet_name='Regex ajout')
+    Data1 = pd.read_excel(Folder + FileRegexExclu, sheet_name='Regex ajout')
     Data1 = Data1[~Data1.UNIVERS.isna()]
     TableParameter = {}
-    TableParameter["Regex"]            = "TEXT"
-    TableParameter["UNIVERS"]          = "VARCHAR (50)"
-    TableParameter["SOUS_UNIVERS"]     = "VARCHAR (100)"
-    TableParameter["NEW_REGEX"]        = "TEXT"
+    TableParameter["Regex"] = "TEXT"
+    TableParameter["UNIVERS"] = "VARCHAR (50)"
+    TableParameter["SOUS_UNIVERS"] = "VARCHAR (100)"
+    TableParameter["NEW_REGEX"] = "TEXT"
 
-    InsertToPostgre(Data1, engine, "REGEX_INCLUDED", "REFERENTIEL", DropTable=True, TableDict=TableParameter)
+    InsertTableIntoDatabase(Data1,
+                            "REGEX_INCLUDED",
+                            "REFERENTIEL",
+                            database_type, database_name,
+                            DropTable=True,
+                            TableDict=TableParameter)
+
     del FileRegexExclu
     del Data1
 
     engine.close()
-
-
-
-
-
-
