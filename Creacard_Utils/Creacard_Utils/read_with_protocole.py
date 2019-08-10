110352bc41ad7c321186484fe8279d9fa88a3051
JustinValet <jv.datamail@gmail.com>
2019-08-10 09:58:57 +0200

    test

--- Creacard_Utils/Creacard_Utils/read_with_protocole.py
+++ Creacard_Utils/Creacard_Utils/read_with_protocole.py
@@ -1,9 +1,12 @@
 from creacard_connectors.ftp_connector import connect_to_FTP
+from creacard_connectors.sftp_connector import connect_to_SFTP
 import pandas as pd
 import os
 import glob
 
-def read_csv_protocole(protocole_type, protocole_name, filepath, csv_params):
+def read_csv_protocole(protocole_type, protocole_name, filepath, csv_params, **kwargs):
+
+    _local_filesystem_path = kwargs.get('copy_to_local', None)
 
     i = 0
     folder_path = ""
@@ -29,6 +32,24 @@ def read_csv_protocole(protocole_type, protocole_name, filepath, csv_params):
         data = connect_to_FTP(protocole_type, protocole_name).extract_csv_from_ftp_to_dataframe(filename,
                                                                                 folder=folder_path,
                                                                                 csv_params=csv_params)
+    elif protocole_type == "SFTP":
+        data = connect_to_SFTP(protocole_type, protocole_name).extract_csv_from_sftp_to_dataframe(filename,
+                                                                                                folder=folder_path,
+                                                                                                csv_params=csv_params)
+
+    if _local_filesystem_path is not None:
+
+        folder_destination = _local_filesystem_path["destination_folder"]
+        csv_destination_params = _local_filesystem_path["csv_destination_params"]
+
+        if csv_destination_params is not None:
+
+            data.to_csv(folder_destination+filename, **csv_destination_params)
+
+        else:
+
+            data.to_csv(folder_destination + filename, index=False)
+
     return data
 
 
@@ -50,6 +71,18 @@ def list_files_protocole(protocole_type, protocole_name, folder):
                                                                              regex=True,
                                                                              case=False)]
         _file_list = _file_list.reset_index(drop=True)
+
+    elif protocole_type == "SFTP":
+
+        _file_list = pd.DataFrame(
+            connect_to_SFTP(protocole_type, protocole_name).get_filenames_from_folder(folder=folder),
+            columns=["FileName"])
+
+        _file_list = _file_list[_file_list["FileName"].astype(str).str.contains('^(.*?)\.csv',
+                                                                             regex=True,
+                                                                             case=False)]
+        _file_list = _file_list.reset_index(drop=True)
+
 
 
     return _file_list
