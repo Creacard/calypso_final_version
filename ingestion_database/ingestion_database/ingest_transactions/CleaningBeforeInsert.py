d367379b805a63bdd0128383bffd9bd339d4a12d
JustinValet <jv.datamail@gmail.com>
2019-08-23 10:09:32 +0200

    test

--- ingestion_database/ingestion_database/ingest_transactions/CleaningBeforeInsert.py
+++ ingestion_database/ingestion_database/ingest_transactions/CleaningBeforeInsert.py
@@ -56,7 +56,7 @@ def cleaning_before(Data, FilePath):
         # Reset the index
         Data = Data.reset_index(drop=True)
 
-        # Change Amount sign after 2018-04-11
+        # Change Amount sign after 2018-04-10
         if DateFile >= pd.to_datetime('2018-04-10'):
             Data.loc[Data["TransactionTP"].isin(
                 ['ATM Domestic', 'ATM International', 'Merchandise Refund Hold Reversals', 'Merchant refunds',
