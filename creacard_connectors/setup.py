e13d2877b8386d2e0261a9db30e376bf14747024
JustinValet <jv.datamail@gmail.com>
2019-08-28 11:44:14 +0200

    test

--- creacard_connectors/setup.py
+++ creacard_connectors/setup.py
@@ -11,5 +11,8 @@ setup(
   url='',
   license='',
   author='Justin Valet',
-  description='This packages wraps several tools'
+  description='This packages wraps several tools',
+  install_requires=[
+    "pysftp"
+  ]
 )
