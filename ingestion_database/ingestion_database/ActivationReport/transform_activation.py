4be67aab116d0302ff720ba5acb9f3f1df9075d2
JustinValet <jv.datamail@gmail.com>
2019-10-23 09:40:23 +0200

    test

--- ingestion_database/ingestion_database/ActivationReport/transform_activation.py
+++ ingestion_database/ingestion_database/ActivationReport/transform_activation.py
@@ -6,7 +6,7 @@ def transform_activation_report(data, filepath, **kwargs):
     FileName = filepath.split('/')[-1]
 
     data["CardholderID"] = data["CardholderID"].astype(str).str.replace("'", "")
-    data["ActivationDate"] = pd.to_datetime(data["ActivationTime"])
+    data["ActivationDate"] = pd.to_datetime(data["ActivationTime"], format="%d/%m/%Y %H:%M:%S")
 
     data = data[["ActivationDate", "CardholderID"]]
     data.columns = ["ActivationTime", "CardHolderID"]
