90b09f1009a27f70fd5505dfd0234e0c0c592768
JustinValet <jv.datamail@gmail.com>
2019-12-12 08:58:49 +0100

    test

---
+++ calypso_ids/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requierements.txt") as f:
+    requierements = f.read()
+
+setup(
+  name='calypso_ids',
+  version = '0.0.1',
+  requierements= requierements,
+  packages=find_packages(),
+  url='',
+  license='',
+  author='Justin Valet',
+  description='This packages wraps several tools',
+)
