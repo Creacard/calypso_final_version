28426512dd224d6ec78fd008994227dc13e0f4fa
JustinValet <jv.datamail@gmail.com>
2019-12-18 15:14:34 +0100

    test

--- calypso_ids/calypso_ids/id_generator.py
+++ calypso_ids/calypso_ids/id_generator.py
@@ -2,7 +2,7 @@ import pandas as pd
 import time
 
 
-def compute_user_id_with_combinaison(data, **kwargs):
+def compute_user_id(data, **kwargs):
 
     max_user_id = kwargs.get('last_user_id', 0)
 
@@ -47,68 +47,6 @@ def compute_user_id_with_combinaison(data, **kwargs):
         non_unique_combi = non_unique_combi.loc[non_unique_combi["USER_ID"] > 1, "combinaison"]
 
         if (len(non_unique_num) > 0) or (len(non_unique_email) > 0) or (len(non_unique_combi) > 0):
-            sorted = False
-        else:
-            sorted = True
-
-        i = i + 1
-
-    toc = time.time() - tic
-
-    new_user_id = pd.DataFrame(user_id["USER_ID"].unique())
-    new_user_id.columns = ["USER_ID"]
-
-    if max_user_id == 0:
-        new_user_id["TMP_USER_ID"] = pd.DataFrame(range(1, len(new_user_id) + 1))
-    else:
-        last_user_id = int(max_user_id) + 1 + len(new_user_id)
-        new_user_id["TMP_USER_ID"] = pd.DataFrame(range(int(max_user_id) + 1, int(last_user_id)))
-
-    user_id = pd.merge(user_id, new_user_id, on="USER_ID", how="inner")
-    user_id["USER_ID"] = user_id["TMP_USER_ID"]
-    user_id = user_id.drop(columns='TMP_USER_ID', axis=1)
-
-    print("generation of user id took {} seconds".format(toc))
-
-    return user_id
-
-
-def compute_user_id_without_combinaison(data, **kwargs):
-
-    max_user_id = kwargs.get('last_user_id', 0)
-
-    tic = time.time()
-
-    user_id = data.copy()
-    id_user = pd.DataFrame(range(1, len(user_id) + 1))
-    user_id["USER_ID"] = id_user
-
-    sorted = False
-    i = 0
-    while sorted is False:
-
-        tmp_user_id = user_id.groupby("NoMobile")["USER_ID"].min().reset_index()
-        tmp_user_id.columns = ["NoMobile", "TMP_USER_ID"]
-        user_id = pd.merge(user_id, tmp_user_id, on="NoMobile", how="inner")
-        user_id["USER_ID"] = user_id["TMP_USER_ID"]
-        user_id = user_id.drop(columns='TMP_USER_ID', axis=1)
-
-        tmp_user_id = user_id[user_id["GoodEmail"] == 1].groupby("Email")["USER_ID"].min().reset_index()
-        tmp_user_id.columns = ["Email", "TMP_USER_ID"]
-        user_id = pd.merge(user_id, tmp_user_id, on="Email", how="left")
-        user_id.loc[~user_id["TMP_USER_ID"].isnull(), "USER_ID"] = user_id["TMP_USER_ID"]
-        user_id = user_id.drop(columns='TMP_USER_ID', axis=1)
-
-        non_unique_num = user_id.groupby("NoMobile")["USER_ID"].nunique().sort_values().reset_index()
-        non_unique_num = non_unique_num.loc[non_unique_num["USER_ID"] > 1, "NoMobile"]
-
-
-        non_unique_email = user_id[user_id["GoodEmail"] == 1].groupby("Email")[
-            "USER_ID"].nunique().sort_values().reset_index()
-        non_unique_email = non_unique_email.loc[non_unique_email["USER_ID"] > 1, "Email"]
-
-
-        if (len(non_unique_num) > 0) or (len(non_unique_email) > 0):
             sorted = False
         else:
             sorted = True
